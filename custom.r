7302adcf1e42ebfb444f9a2484e8d2371e74c365
Zhilei Ma <80358105+zmaalgorithmia@users.noreply.github.com>
2022-12-19 10:53:55 -0500

    V1.15 on DataRobot

--- custom.r
+++ custom.r
@@ -33,6 +33,6 @@
 score <- function(data, model){
     pred.model <- predict(model, newdata = data, type = "response")
     predictions <- data.frame(pred.model, 1-pred.model)
-    # colnames(predictions)<-c("1","0")
+    colnames(predictions)<-c("1","0")
     return(predictions)
 }
